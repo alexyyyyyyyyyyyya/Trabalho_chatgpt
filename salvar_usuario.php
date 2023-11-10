09e237409afb6731f259fc4ea77222a1845185ff
alexyyyyyyyyyyyya <alexya.2021319318@aluno.iffar.edu.br>
2023-11-10 09:14:01 -0300

    Update salvar_usuario.php
    salva o usuario

--- salvar_usuario.php
+++ salvar_usuario.php
@@ -3,13 +3,12 @@ include 'conexao.php';
 
 $id = $_POST['id'];
 $nome = $_POST['nome'];
-$email = $_POST['email'];
 $senha = $_POST['senha'];
 
 if ($id == '') {
-    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+    $sql = "INSERT INTO usuarios (nome, senha) VALUES ('$nome','$senha')";
 } else {
-    $sql = "UPDATE usuarios SET nome='$nome', email='$email', senha='$senha' WHERE id=$id";
+    $sql = "UPDATE usuarios SET nome='$nome', senha='$senha' WHERE id=$id";
 }
 
 $conn->query($sql);
